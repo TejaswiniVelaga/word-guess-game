75046a5a5559b5023f2dfcd1b5577eab7b4a63cf
TejaswiniVelaga <tejaswinivelaga9@gmail.com>
2025-09-05 17:16:03 +0530

    Removed unnecessary images and gif file

--- gif.py
+++
@@ -1,6 +0,0 @@
-import imageio
-filenames=['cat1.png', 'cat2.png']
-images=[]
-for filename in filenames:
-    images.append(imageio.imread(filename))
-imageio.imwrite('cat.gif', images, duration = 500, loop =0)
